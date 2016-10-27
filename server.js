e4248e91a2906dfee5e10c3eb7ddf435f48853ae
Niranjan Ravichandran <niranjanrav@gmail.com>
2016-10-27 19:14:10 -0400

    Changed URI

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-var FitbitStrategy = require( 'passport-fitbit-oauth2' ).FitbitOAuth2Strategy;;
+var FitbitStrategy = require( 'passport-fitbit-oauth2' ).FitbitOAuth2Strategy;
+var FITBIT_CLIENT_ID = "227WYF";
+var FITBIT_CLIENT_SECRET = "28e96c62a095734ac15d667c085ce2dc";
 
 passport.use(new FitbitStrategy({
         clientID:     FITBIT_CLIENT_ID,
         clientSecret: FITBIT_CLIENT_SECRET,
-        callbackURL: "http://yourdormain:3000/auth/fitbit/callback"
+        callbackURL: "http://52.89.68.106:8080/auth/fitbit/callback"
     },
     function(accessToken, refreshToken, profile, done) {
         User.findOrCreate({ fitbitId: profile.id }, function (err, user) {
