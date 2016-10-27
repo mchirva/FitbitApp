fe9832c823e012de6f853c3f10db77c7235beea4
Mounica Chirva <mounicachirva@gmail.com>
2016-10-27 19:36:03 -0400

    Printing callback data

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 var FitbitStrategy = require( 'passport-fitbit-oauth2' ).FitbitOAuth2Strategy;
+var passport = require('passport');
+var express = require('express');
+
+var app = express();
+
 var FITBIT_CLIENT_ID = "227WYF";
 var FITBIT_CLIENT_SECRET = "28e96c62a095734ac15d667c085ce2dc";
+
+var router = express.Router();
 
 passport.use(new FitbitStrategy({
         clientID:     FITBIT_CLIENT_ID,
@@ -13,3 +20,18 @@ passport.use(new FitbitStrategy({
         });
     }
 ));
+
+app.get('/auth/fitbit',
+    passport.authenticate('fitbit', { scope: ['activity','heartrate','location','profile'] }
+    ));
+
+app.get( '/auth/fitbit/callback/:code', passport.authenticate( 'fitbit', {
+    failureRedirect: '/auth/fitbit/failure'
+}), function(req, res) {
+    console.log(req);
+    res.redirect('/app.html');
+});
+
+app.listen(8080, function() {
+    console.log("✔ Express server listening on port %d in %s mode", 8080, app.get('env'));
+});
