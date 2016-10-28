f1deaa1c4e6307919deb60b752bd1876043355c8
Mounica Chirva <mounicachirva@gmail.com>
2016-10-27 21:06:46 -0400

    Added View

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var FitbitStrategy = require( 'passport-fitbit-oauth2' ).FitbitOAuth2Strategy;
 var passport = require('passport');
 var express = require('express');
+var request = require('request');
 
 var app = express();
 
@@ -9,12 +10,17 @@ var FITBIT_CLIENT_SECRET = "28e96c62a095734ac15d667c085ce2dc";
 
 var router = express.Router();
 
+var token = '';
+var profileId = '';
+
 passport.use(new FitbitStrategy({
         clientID:     FITBIT_CLIENT_ID,
         clientSecret: FITBIT_CLIENT_SECRET,
         callbackURL: "http://52.89.68.106:8080/auth/fitbit/callback"
     },
     function(req, accessToken, refreshToken, profile, done) {
+        token = accessToken;
+        profileId = profile.id;
         return done(req, user);
     }
 ));
@@ -24,8 +30,11 @@ app.get('/auth/fitbit',
     ));
 
 app.get( '/auth/fitbit/callback', function(req, user, res) {
-    console.log('####',req);
-    // res.redirect('/app.html');
+    request('https://api.fitbit.com/1/user/'+profileId+'/activities/date/2016-01-26.json', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            res.render('activity', {error: false, body: body});
+        }
+    })
 });
 
 app.listen(8080, function() {
