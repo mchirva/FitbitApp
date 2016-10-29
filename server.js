e87e09f2701c7887fd443ced057b0ed54c1792d2
Mounica Chirva <mounicachirva@gmail.com>
2016-10-29 15:51:02 -0400

    Testing with render

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ passport.use(new FitbitStrategy({
         token = accessToken;
         profileId = profile.id;
         console.log('token-'+token);
-        return done(null, user);
+        return done(null, accessToken);
     }
 ));
 
