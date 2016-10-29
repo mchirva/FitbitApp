c3d626512c4ac5c9af97212a15162f53375cfcef
Mounica Chirva <mounicachirva@gmail.com>
2016-10-29 14:14:26 -0400

    Added header for authorization

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ passport.use(new FitbitStrategy({
 app.get('/auth/fitbit', passport.authenticate('fitbit', { scope: ['activity','heartrate','location','profile'] }
     ));
 
-app.get( '/auth/fitbit/callback', function(req, user, res) {
+app.get( '/auth/fitbit/callback', passport.authenticate('fitbit',function(req, user, res) {
     console.log('Callback');
     console.log('token-'+req.query.code, user);
     var options = {
