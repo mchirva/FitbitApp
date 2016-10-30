7343ebc4367adf960099251cc7fe02e74d3888d8
Mounica Chirva <mounicachirva@gmail.com>
2016-10-30 18:22:45 -0400

    Added views

--- views/home.js
+++ views/home.js
@@ -1,39 +1,79 @@
 $(document).ready(function() {
-	var response = $.ajax({
-       url: "http://52.89.68.106:8080/auth/fitbit/",
-       method: "get",
-       success:function(response){
-       		console.log(response);
-       		
-        },
-        error: function(XMLHttpRequest, textStatus, errorThrown) { 
-            alert("Status: " + textStatus); alert("Error: " + errorThrown); 
-        } 
-    });
-	// var response = $.ajax({
- //       url: "https://api.fitbit.com/1/user/3CH54G/activities/date/2016-01-26.json",
- //       method: "get",
- //       dataType:'json',
- //       headers: {
- //        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIzQ0g1NEciLCJhdWQiOiIyMjdXWUYiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyYWN0IHJsb2MgcmhyIHJwcm8iLCJleHAiOjE0Nzc2NDIyNDgsImlhdCI6MTQ3NzYxMzQ0OH0.pApKwW0RXKzddGTpKVeS5BXA2ncLpANW5u--0wgPmaE'
- //       },
- //       success:function(response){
- //       		//console.log(JSON.stringify(response));
- //       		var data = {}; 
- //       		var goals=[];
- //       		data.goals=goals;
- //       		var goal=[
- //       			distance:response.goals.distance
- //       		];
- //       		data.goals.push(goal);
+	var data = [4, 8, 15, 16, 23, 42];
+	var response={
+	  "activities-heart": [
+	    {
+	      "dateTime": "2016-10-29",
+	      "value": {
+	        "customHeartRateZones": [],
+	        "heartRateZones": [
+	          {
+	            "max": 93,
+	            "min": 30,
+	            "name": "Out of Range"
+	          },
+	          {
+	            "max": 130,
+	            "min": 93,
+	            "name": "Fat Burn"
+	          },
+	          {
+	            "max": 158,
+	            "min": 130,
+	            "name": "Cardio"
+	          },
+	          {
+	            "max": 220,
+	            "min": 158,
+	            "name": "Peak"
+	          }
+	        ]
+	      }
+	    }
+	  ]
+	}
 
- //       		console.log(JSON.stringify(data));
- //       		$('#fitbitTable').DataTable({
- //       			"ajax": JSON.stringify(data.goals)
- //        	});
- //        },
- //        error: function(XMLHttpRequest, textStatus, errorThrown) { 
- //            alert("Status: " + textStatus); alert("Error: " + errorThrown); 
- //        } 
- //    });
+var l=response["activities-heart"][0].value.heartRateZones.length;
+maxRate=[]
+for(var i=0;i<l;i++){
+	maxRate[i]=response["activities-heart"][0].value.heartRateZones[i].max;
+}
+label=[]
+for(var i=0;i<l;i++){
+	label[i]=response["activities-heart"][0].value.heartRateZones[i].name;
+}
+minRate=[]
+for(var i=0;i<l;i++){
+	minRate[i]=response["activities-heart"][0].value.heartRateZones[i].min;
+}
+
+	var x = d3.scaleLinear()
+    .domain([d3.min(maxRate), d3.max(maxRate)])
+    .range([d3.min(maxRate), d3.max(maxRate) * 2]);
+
+    var y = d3.scaleLinear()
+    .domain([d3.min(minRate), d3.max(minRate)])
+    .range([d3.min(minRate), d3.max(minRate) * 2]);
+
+console.log(label);
+d3.select(".maxChart")
+  .selectAll("div")
+    .data(maxRate)
+  .enter().append("div")
+    .style("width", function(d) { return x(d) + "px"; })
+    .text(function(d) { return d; });
+
+d3.select(".label")
+  .selectAll("bar")
+    .data(label)
+  .enter().append("div")
+    .text(function(d) { return d; });
+
+d3.select(".minChart")
+  .selectAll("div")
+    .data(minRate)
+  .enter().append("div")
+    .style("width", function(d) { return y(d) + "px"; })
+    .text(function(d) { return d; });
 });
+
