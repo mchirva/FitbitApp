a4540a03c2b8485d10b382bc64aa52939887ced0
Mounica Chirva <mounicachirva@gmail.com>
2016-11-02 21:16:52 -0400

    Implemented logout functionality

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var plotly = require('plotly')('fitbit','6mcgrwqpjf');
 var fs = require('fs');
 
 var app = express();
-
+var loggedIn = false;
 //AWS
 var FITBIT_CLIENT_ID = "227WYF";
 var FITBIT_CLIENT_SECRET = "28e96c62a095734ac15d667c085ce2dc";
@@ -53,92 +53,104 @@ app.get( '/auth/fitbit/callback', passport.authenticate('fitbit'),
     function(req, res) {
         tokenId =  req.user.token;
         profileId = req.user.profile.id;
+        loggedIn = "true";
         res.redirect('/heartRate');
     }
 );
 
 app.get('/heartRate', function (req, res) {
-    var bearer = 'Bearer '+ tokenId;
-    var options = {
-        method: 'GET',
-        url: 'https://api.fitbit.com/1/user/'+profileId+'/activities/heart/date/today/today.json',
-        headers: {
-            'Authorization': bearer
-        }
-    };
-    request(options)
-        .then( function (response) {
-            var responseJSON = JSON.parse(response);
-            
-            var heartRateZonesArray = responseJSON["activities-heart"][0].value.heartRateZones;
+    if(loggedIn == "true") {
+        var bearer = 'Bearer ' + tokenId;
+        var options = {
+            method: 'GET',
+            url: 'https://api.fitbit.com/1/user/' + profileId + '/activities/heart/date/today/today.json',
+            headers: {
+                'Authorization': bearer
+            }
+        };
+        request(options)
+            .then(function (response) {
+                var responseJSON = JSON.parse(response);
 
-            //plotting the graph
-            var minimum = {
-                x: [heartRateZonesArray[0].name, heartRateZonesArray[1].name, heartRateZonesArray[2].name, heartRateZonesArray[3].name],
-                y: [heartRateZonesArray[0].min, heartRateZonesArray[1].min, heartRateZonesArray[2].min, heartRateZonesArray[3].min],
-                name: "Minimum",
-                type: "bar"
-            };
-            var maximum = {
-                x: [heartRateZonesArray[0].name, heartRateZonesArray[1].name, heartRateZonesArray[2].name, heartRateZonesArray[3].name],
-                y: [heartRateZonesArray[0].max, heartRateZonesArray[1].max, heartRateZonesArray[2].max, heartRateZonesArray[3].max],
-                name: "Minimum",
-                type: "bar"
-            };
-            
-            var data = [minimum, maximum];
-            var layout = {barmode: "group"};
-            var graphOptions = {layout: layout, filename: "heartRate-bar", fileopt: "overwrite"};
-            plotly.plot(data, graphOptions, function (err, msg) {
-                if(!err) {
-                    console.log(msg);
-                    res.render('activity', {page: "heartRate", response: heartRateZonesArray});
-                }
-                else console.log(err);
+                var heartRateZonesArray = responseJSON["activities-heart"][0].value.heartRateZones;
+
+                //plotting the graph
+                var minimum = {
+                    x: [heartRateZonesArray[0].name, heartRateZonesArray[1].name, heartRateZonesArray[2].name, heartRateZonesArray[3].name],
+                    y: [heartRateZonesArray[0].min, heartRateZonesArray[1].min, heartRateZonesArray[2].min, heartRateZonesArray[3].min],
+                    name: "Minimum",
+                    type: "bar"
+                };
+                var maximum = {
+                    x: [heartRateZonesArray[0].name, heartRateZonesArray[1].name, heartRateZonesArray[2].name, heartRateZonesArray[3].name],
+                    y: [heartRateZonesArray[0].max, heartRateZonesArray[1].max, heartRateZonesArray[2].max, heartRateZonesArray[3].max],
+                    name: "Minimum",
+                    type: "bar"
+                };
+
+                var data = [minimum, maximum];
+                var layout = {barmode: "group"};
+                var graphOptions = {layout: layout, filename: "heartRate-bar", fileopt: "overwrite"};
+                plotly.plot(data, graphOptions, function (err, msg) {
+                    if (!err) {
+                        console.log(msg);
+                        res.render('activity', {page: "heartRate", response: heartRateZonesArray});
+                    }
+                    else console.log(err);
+                });
+            })
+            .catch(function (error) {
+                console.log(error);
+                res.redirect('/');
             });
-        })
-        .catch( function (error) {
-            console.log(error);
-            res.redirect('/');            
-        });
+    }
+    else {
+        res.redirect('/');
+    }
 });
 
 app.get('/logout', function (req, res){
+        loggedIn = "false";
         req.logout();
         res.redirect('/');
 });
 
 app.get('/steps', function (req, res){
-    var bearer = 'Bearer '+ tokenId;
-    var options = {
-        method: 'GET',
-        url: 'https://api.fitbit.com/1/user/'+profileId+'/activities/date/2016-10-29.json',
-        headers: {
-            'Authorization': bearer
-        }
-    };
-    request(options)
-        .then( function (response) {
-            var responseJSON = JSON.parse(response);
-            console.log(responseJSON.goals);
-            var data = [{
-                            x: ["goal", "activity"],
-                            y: [responseJSON.goals.steps, responseJSON.summary.steps],
-                            type: "bar"
-                        }];
-            var graphOptions = {filename: "steps-bar", fileopt: "overwrite"};
-            plotly.plot(data, graphOptions, function (err, msg) {
-                if(!err) {
-                    console.log(msg);
-                    res.render('activity', {page: "steps", response: responseJSON});
-                }
-                else console.log(err);
-            });            
-        })
-        .catch( function (error) {
-            console.log(error);
-            res.redirect('/');
-        });
+    if(loggedIn == "true") {
+        var bearer = 'Bearer ' + tokenId;
+        var options = {
+            method: 'GET',
+            url: 'https://api.fitbit.com/1/user/' + profileId + '/activities/date/2016-10-29.json',
+            headers: {
+                'Authorization': bearer
+            }
+        };
+        request(options)
+            .then(function (response) {
+                var responseJSON = JSON.parse(response);
+                console.log(responseJSON.goals);
+                var data = [{
+                    x: ["goal", "activity"],
+                    y: [responseJSON.goals.steps, responseJSON.summary.steps],
+                    type: "bar"
+                }];
+                var graphOptions = {filename: "steps-bar", fileopt: "overwrite"};
+                plotly.plot(data, graphOptions, function (err, msg) {
+                    if (!err) {
+                        console.log(msg);
+                        res.render('activity', {page: "steps", response: responseJSON});
+                    }
+                    else console.log(err);
+                });
+            })
+            .catch(function (error) {
+                console.log(error);
+                res.redirect('/');
+            });
+    }
+    else {
+        res.redirect('/');
+    }
 });
 
 app.get('/', function(req, res) {
